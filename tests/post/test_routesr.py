335ef66f9c0b222609d19f9fe9cdeee9ad0d1ada
Oleksandr Savchuk <oleksandr.savchuk@lasoft.org>
2024-10-10 18:43:25 +0300

    update Post test

--- tests/post/test_routesr.py
+++ tests/post/test_routesr.py
@@ -1,10 +1,14 @@
 import pytest
 import pytest_asyncio
+from bson import ObjectId
 from httpx import AsyncClient, ASGITransport
+from faker import Faker
 
 from database import user_collection
 from database import post_collection
 from main import app
+
+faker = Faker()
 
 
 @pytest_asyncio.fixture(scope='function')
@@ -24,7 +28,7 @@ async def test_user():
 @pytest_asyncio.fixture(scope='function')
 async def test_post(test_user):
     post_data = {
-        "_id": "test_post_id",
+        "_id": ObjectId(),
         "title": "Fixture Test Post",
         "content": "Content of the fixture post.",
         "user_id": test_user['_id']
@@ -33,9 +37,26 @@ async def test_post(test_user):
     yield post_data
 
 
+@pytest_asyncio.fixture(scope='function')
+async def test_posts(test_user):
+    posts_data = []
+
+    for _ in range(3):
+        post_data = {
+            "_id": ObjectId(),
+            "title": faker.sentence(nb_words=5),
+            "content": faker.text(max_nb_chars=50),
+            "user_id": test_user['_id']
+        }
+        posts_data.append(post_data)
+
+    await post_collection.insert_many(posts_data)
+    yield posts_data
+
+
 @pytest.mark.positive
 @pytest.mark.asyncio
-async def test_list_posts(test_user, test_post):
+async def test_list_posts(test_user, test_posts):
     async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as async_client:
         response = await async_client.get("/posts/")
         posts_list = response.json()
