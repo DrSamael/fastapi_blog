d52f0cd230c361da2cf468bf2621cd9802ac66fa
Oleksandr Savchuk <oleksandr.savchuk@lasoft.org>
2024-10-30 18:08:25 +0200

    fix whitespaces

--- seed.py
+++ seed.py
@@ -1,7 +1,6 @@
 import asyncio
 from user.crud import retrieve_user_by_email, add_user
 
-
 UserData = {
     "email": "superadmin@user.com",
     "password": "123123",
@@ -17,11 +16,13 @@ async def create_admin_user():
     await add_user(UserData)
     print("Admin user created successfully")
 
+
 async def is_admin_user_present():
     if await retrieve_user_by_email("superadmin@user.com"):
         print("Admin user already exists. No new user created")
         return True
     return False
+
 
 if __name__ == "__main__":
     asyncio.run(create_admin_user())
