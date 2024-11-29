dbf9c50c5138a034502873aafffa6f74c575634e
Oleksandr Savchuk <oleksandr.savchuk@lasoft.org>
2024-11-29 15:40:07 +0200

    change docker database

--- src/database.py
+++ src/database.py
@@ -1,7 +1,7 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 import os
 
-DATABASE_NAME = "fastapi_test_db" if os.getenv("TESTING") == "true" else "fastapi_db"
+DATABASE_NAME = "fastapi_test_db" if os.getenv("TESTING") == "true" else os.getenv("DATABASE_NAME")
 DATABASE_URI = os.getenv("DATABASE_URI", "mongodb://mongo:27017")
 
 client = AsyncIOMotorClient(DATABASE_URI)
